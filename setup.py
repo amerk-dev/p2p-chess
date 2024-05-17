b656f581ffd190abea786a87f32efef4c9d3888c
p2p-chess <devamerkh@gmail.com>
2024-05-17 14:04:02 +0500

    upgrade setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,15 @@
 from cx_Freeze import setup, Executable
 import sys
+import os
+
+# Получение пути к библиотекам Tcl/Tk
+tcl_library = os.path.join(sys.base_prefix, 'tcl', 'tcl8.6')
+tk_library = os.path.join(sys.base_prefix, 'tcl', 'tk8.6')
+python_dlls_path = os.path.join(sys.base_prefix, 'DLLs')
+
+# Установка переменных окружения для Tcl/Tk
+os.environ['TCL_LIBRARY'] = tcl_library
+os.environ['TK_LIBRARY'] = tk_library
 
 # Список дополнительных файлов, которые нужно включить в сборку
 include_files = [
@@ -14,24 +24,28 @@ include_files = [
     ('./piecess/white_knight.png', 'piecess/white_knight.png'),
     ('./piecess/white_pawn.png', 'piecess/white_pawn.png'),
     ('./piecess/white_queen.png', 'piecess/white_queen.png'),
-    ('./piecess/white_rook.png', 'piecess/white_rook.png'), #
-    ('./chess_pieces/black_bishop.png', 'chess_pieces/black_bishop.png'),
-    ('./chess_pieces/black_king.png', 'chess_pieces/black_king.png'),
-    ('./chess_pieces/black_knight.png', 'chess_pieces/black_knight.png'),
-    ('./chess_pieces/black_pawn.png', 'chess_pieces/black_pawn.png'),
-    ('./chess_pieces/black_queen.png', 'chess_pieces/black_queen.png'),
-    ('./chess_pieces/black_rook.png', 'chess_pieces/black_rook.png'),
-    ('./chess_pieces/white_bishop.png', 'chess_pieces/white_bishop.png'),
-    ('./chess_pieces/white_king.png', 'chess_pieces/white_king.png'),
-    ('./chess_pieces/white_knight.png', 'chess_pieces/white_knight.png'),
-    ('./chess_pieces/white_pawn.png', 'chess_pieces/white_pawn.png'),
-    ('./chess_pieces/white_queen.png', 'chess_pieces/white_queen.png'),
-    ('./chess_pieces/white_rook.png', 'chess_pieces/white_rook.png'),
+    ('./piecess/white_rook.png', 'piecess/white_rook.png'),
+    ('./chess_pieces/black_bishop.svg', 'chess_pieces/black_bishop.svg'),
+    ('./chess_pieces/black_king.svg', 'chess_pieces/black_king.svg'),
+    ('./chess_pieces/black_knight.svg', 'chess_pieces/black_knight.svg'),
+    ('./chess_pieces/black_pawn.svg', 'chess_pieces/black_pawn.svg'),
+    ('./chess_pieces/black_queen.svg', 'chess_pieces/black_queen.svg'),
+    ('./chess_pieces/black_rook.svg', 'chess_pieces/black_rook.svg'),
+    ('./chess_pieces/white_bishop.svg', 'chess_pieces/white_bishop.svg'),
+    ('./chess_pieces/white_king.svg', 'chess_pieces/white_king.svg'),
+    ('./chess_pieces/white_knight.svg', 'chess_pieces/white_knight.svg'),
+    ('./chess_pieces/white_pawn.svg', 'chess_pieces/white_pawn.svg'),
+    ('./chess_pieces/white_queen.svg', 'chess_pieces/white_queen.svg'),
+    ('./chess_pieces/white_rook.svg', 'chess_pieces/white_rook.svg'),
     ('board.py', '.'),
     ('board2.py', '.'),
     ('client.py', '.'),
     ('pieces.py', '.'),
-    ('player.py', '.')
+    ('player.py', '.'),
+    (os.path.join(python_dlls_path, 'tk86t.dll'), 'tk86t.dll'),
+    (os.path.join(python_dlls_path, 'tcl86t.dll'), 'tcl86t.dll'),
+    (tcl_library, 'lib/tcl8.6'),
+    (tk_library, 'lib/tk8.6')
 ]
 
 # Определите базу (только для Windows)
@@ -42,6 +56,7 @@ if sys.platform == "win32":
 # Список используемых пакетов
 packages = [
     "pygame",
+    "tkinter",
 ]
 
 # Создайте объект Executable
